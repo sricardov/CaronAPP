069f9546a5a39ba236a21de448d0bc2fce92a75f
EPERSON <epersoncmxf@gmail.com>
2020-12-02 20:07:05 -0300

    Projeto adicionado

---
+++ src/me/br/caronapp/Console.java
@@ -0,0 +1,23 @@
+package me.br.caronapp;
+
+import java.util.Scanner;
+
+public class Console {
+	
+	Scanner scan;
+	int stage = 0;
+	
+	public Console() {
+		scan = new Scanner(System.in);
+		stage = 0;
+	}
+	
+	public boolean draw() {
+		
+		
+		
+		
+		return true;
+	}
+	
+}
